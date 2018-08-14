f935a3f056846c76887667d1650a223a4805f7e8
fturazzini <fturazzini@q4tech.com>
2018-08-14 14:16:54 -0300

    Se hizo unos cambios en el home y se pasaron los archivos .html a .php

--- index.php
+++ index.php
@@ -7,19 +7,20 @@
     <!-- <link rel="stylesheet" href="css/estilos.css"> -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/estilos.css">
 	<title>E-Commerce</title>
 
 </head>
 <body>
 
 <div class="container-fluid">
-	<!-- Implementacion del Nav Bar -->
+	<!-- Nav Bar -->
 	<ul class="nav">
   		<li class="nav-item">
     		<a class="nav-link active" href="#">Active</a>
   		</li>
   		<li class="nav-item">
-    		<a class="nav-link" href="formulario.html" target="_blank">Formulario</a>
+    		<a class="nav-link" href="formulario.php" target="_blank">Formulario</a>
   		</li>
   		<li class="nav-item">
     		<a class="nav-link" href="#">Link</a>
@@ -29,8 +30,25 @@
   		</li>
 	</ul>
 </div>
+<!-- banner -->
+    <section id="banner">
+      <div class="container">
+        <div class="row">
+         <!--Aquí forzo a que siempre se vea en 12 columnas 
+         aunque sea cualquier dispositivo como lo que realice
+         en el formulario que les envie-->
+          <div class="col-xs-12">
+            <div class="intro-text text-center">
+              <h2>Bienvenidos</h2>
+              <h1>E-Commerce - Fullstack</h1>
+              <a href="#services" class="btn btn-lg">COMPRAR</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
-
+  
 
 	<script src="js/bootstrap.min.js"></script>
 </body>
