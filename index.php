e77c12c386cf43c70bcb0a92cc9d9f2e63418903
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-08-17 22:48:59 -0300

    actualizado con branch nelson

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php 
+// Validaciones aca
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="stylesheet" href="css/estilos.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/master.css">
+    <!-- <link rel="stylesheet" href="css/estilos.css"> -->
+	<title>E-Commerce</title>
+
+</head>
+<body>
+
+<!-- NAV -->
+  <div class="menu">
+    <img style="width:103px; height:66px;" src="images/logo.jpg" alt="" class="logo">
+    <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sesion.php">Iniciar Sesión</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="preguntasfrecuentes.php">Faq</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registro.php" target="_blank">Registrarme</a>
+        </li>
+    </ul>
+    <div class="icono-carrito">
+      <img src="images/carrito-icono.png" alt="Mis Compras">
+    </div>
+  </div>
+<!-- FIN NAV -->
+
+<!-- BANNER -->
+  <div class="banner-home">
+    <img src="images/slider-home.png" alt="">
+  </div>
+<!-- FIN BANNER -->
+
+<!-- PRODUCTOS bootstrap-->
+<section class="seccion-main">
+  <h3>Lo último de la tienda</h3>
+  <section class="seccion-productos">
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+  </section>
+</section>
+<!-- FIN PRODUCTOS -->
+
+</body>
+</html>
+
