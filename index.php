f2821a54ad7cab8dedbf6a5933f6c9fbf1d75984
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-08-17 23:39:19 -0300

    nuevas img de productos

--- index.php
+++ index.php
@@ -50,15 +50,15 @@
   <h3>Lo último de la tienda</h3>
   <section class="seccion-productos">
     <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
+    <article class="producto"><img src="images/producto-2.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-3.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-4.jpeg" alt=""></article>
+    <article class="producto"><img src="images/producto-5.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-6.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-7.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-8.jpg" alt=""></article>
+    <article class="producto"><img src="images/producto-9.jpeg" alt=""></article>
+    <article class="producto"><img src="images/producto-10.jpg" alt=""></article>
   </section>
 </section>
 <!-- FIN PRODUCTOS -->
