650dda6c1b5f8555899586c226fda2598ba2b493
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-10 13:13:01 -0300

    navbar.php incluede en index

--- index.php
+++ index.php
@@ -7,26 +7,7 @@
 <body>
 
 <!-- NAV -->
-  <div class="menu">
-    <img style="width:103px; height:66px;" src="images/logo.jpg" alt="" class="logo">
-    <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="sesion.php">Iniciar Sesión</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="preguntasfrecuentes.php">Faq</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="registro.php">Registrarme</a>
-        </li>
-    </ul>
-    <div class="icono-carrito">
-      <a href="#"><img src="images/carrito-icono.png" alt="Mis Compras"></a>
-    </div>
-  </div>
+<?php include_once('navbar.php'); ?>
 <!-- FIN NAV -->
 
 <!-- BANNER -->
