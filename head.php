4e68c159bf4f461951809b4cd77807472b150f3b
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-10 13:54:36 -0300

    agregado estilos de preguntasfrecuentes.php

--- head.php
+++ head.php
@@ -4,6 +4,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- <link rel="stylesheet" href="css/estilos.css"> -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/stylepreguntas.css">
     <link rel="stylesheet" href="css/master.css">
     <!-- <link rel="stylesheet" href="css/estilos.css"> -->
 	<title>E-commerce</title>
