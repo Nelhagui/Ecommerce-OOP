6b87a6f4dd6ad31716755f23fd0ec6810d7e9c57
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-10 13:22:44 -0300

    main.php e incluede en index

--- index.php
+++ index.php
@@ -7,24 +7,8 @@
 <body>
   <?php include_once('navbar.php'); ?>
   <?php include_once('banner.php'); ?>
+  <?php include_once('main.php'); ?>
 
-<!-- PRODUCTOS bootstrap-->
-<section class="seccion-main">
-  <h3>Lo último de la tienda</h3>
-  <section class="seccion-productos">
-    <article class="producto"><img src="images/producto-1.png" alt=""></article>
-    <article class="producto"><img src="images/producto-2.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-3.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-4.jpeg" alt=""></article>
-    <article class="producto"><img src="images/producto-5.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-6.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-7.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-8.jpg" alt=""></article>
-    <article class="producto"><img src="images/producto-9.jpeg" alt=""></article>
-    <article class="producto"><img src="images/producto-10.jpg" alt=""></article>
-  </section>
-</section>
-<!-- FIN PRODUCTOS -->
 
 </body>
 </html>
