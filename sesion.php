1bec3a8623752df5b4980d9cbb445bbfcd586705
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-23 05:12:52 -0300

    agregué helpers, loader y 'name' a los inputs (atentos muchachos)

--- sesion.php
+++ sesion.php
@@ -1,30 +1,38 @@
-<!DOCTYPE html>
-
 <?php 
+include 'loader.php'; // ESTE ARCHIVO CONTIENE LOS INCLUEDES DE LAS CLASES
+// QUE ANTES ESTABAN COMO FUNCIONES EN FUNCIONES.PHP
 
-include_once('head.php');
-include_once('controllers/sessionController.php');
+include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()                   
+
+if($auth->check()) {  // ACÁ ESTÁ VERIFIFANDO SI EXISTE UNA SESIÓN, LO VERIFICA EN LA FUNCIÓN 'check()' DE LA CLASE "AUTH.PHP"
+    redirect('perfil.php'); // SI EXISTE LO LLEVA DIRECTO A PERFIL.PHP
+}
 
 if($_POST) {
-    //inicioSesion();
-    // $usuario = $usersDb->dbEmailSearch($_POST['email']);
-    // // if($usuario !== null) {
-    // //     if(password_verify($_POST['password'], $usuario['password']) == true) {
-    // //         $email = $_POST['email'];
-    // //         $auth->login($email);
-    // //         redirect('perfil.php');
-    // //     } 
-    // // }
-    capturaDatos($_POST);
-    if (isset($_POST['recordar'])){
-        recordarPassword($_POST);
+    $usuario = $usersDb->dbEmailSearch($_POST['email']); // ENVÍA EL EMAIL A LA FUNCIÓN 'dbEmailSearch($email) DE LA CLASE 'JSONDB.PHP' PARA VERIFICAR SI EXISTE UN USUARIO CON ESE MAIL EN LA BASE DE DATOS
+    if($usuario !== null) { 
+        if(password_verify($_POST['password'], $usuario['password']) == true) { // COMPRUEBA SI LA CONTRASEÑA COINCIDE USANDO LA FUNCIÓN 'password_verify' DE PHP
+            $email = $_POST['email'];
+            $auth->login($email); // UTILIZA EL EMAIL PARA INICIAR SESION Y COOKIE DESDE LA FUNCIÓN 'login($mail)' DE LA CLASE "AUTH.PHP"
+            redirect('perfil.php');
+        } 
     }
-    header('Location: main-index.php');
     
 }
 
 ?>
-    <title>Iniciar Sesión</title>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="stylesheet" href="css/estilos.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/master.css">
+  <title>Inciar Sesión</title>
 </head>
 
 <body>
@@ -32,13 +40,13 @@ if($_POST) {
 
     <div class="container-fluid">
         <div class="row">
-            <form class="form-signin" action="#" method="POST">
+            <form class="form-signin" action="" method="post">
                 <h2>Ingrese sus datos</h2>
                 <div class="form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su Email">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' placeholder="Ingrese su Email">
                 </div>
                 <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su Contraseña">
+                <input type="password" class="form-control" id="exampleInputPassword1" name='password' placeholder="Ingrese su Contraseña">
                 </div>
                 <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
