d9bb91ca12998813cd77163ab7da29dfe5037dc1
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-23 06:09:54 -0300

    agregué condiciones para negar acceso según session

--- perfil.php
+++ perfil.php
@@ -4,6 +4,7 @@ include 'loader.php'; // ESTE ARCHIVO CONTIENE LOS INCLUEDES DE LAS CLASES
 
 include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()                   
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'?>
@@ -11,6 +12,14 @@ include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()
 </head>
 <body>
     <?php include_once('navbar.php'); ?>
-    <?php echo "Hola, estás en mi perfil" ?>
+
+    <?php if(!$auth->check()):?>
+            <div class="alert alert-danger" role="alert">
+                No estás autorizado/a en esta sección <a href="registro.php" class="alert-link">Registrate</a> o <a href="sesion.php" class="alert-link">Iniciá Sesión</a>
+            </div>
+    <?php else: ?>
+            <?php echo "Hola, estás en mi perfil" ?>
+    <?php endif; ?>
+
 </body>
 </html>
