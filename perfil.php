888fdf1a69c93bcdbc348745225b2681c05be815
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-23 05:44:24 -0300

    agregué include loader.php y helpers.php

--- perfil.php
+++ perfil.php
@@ -1,3 +1,9 @@
+<?php
+include 'loader.php'; // ESTE ARCHIVO CONTIENE LOS INCLUEDES DE LAS CLASES
+    // QUE ANTES ESTABAN COMO FUNCIONES EN FUNCIONES.PHP
+
+include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()                   
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'?>
