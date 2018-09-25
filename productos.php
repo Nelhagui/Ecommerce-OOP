ace7a9e7b6255ae15ead71b1de6339fbcf68e8d3
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-25 11:27:37 -0300

    cambio icono carrito, corrección variables productos.php

--- productos.php
+++ productos.php
@@ -5,8 +5,8 @@ include 'loader.php'; // ESTE ARCHIVO CONTIENE LOS INCLUEDES DE LAS CLASES
 include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()                   
 
 if ($_POST){
-    $producto = $productDb->userArrayProducto($_POST); // CREO UN USUARIO CON LA FUNCIÓN 'userArray($datos)' QUE ESTÁ DENTRO DE LA INSTANCIA '$usersDb' DE LA CLASE 'JSONDB.PHP' HECHA EN 'LOADER.PHP'
-    $usersDb->saveProduct($producto); // GUARDO EL USUARIO CON LA FUNCIÓN 'saveUser($usuario)' QUE ESTÁ DENTRO DE LA INSTANCIA '$usersDb' DE LA CLASE 'JSONDB.PHP' HECHA EN 'LOADER.PHP'
+    $producto = $productDb->userArrayProducto($_POST); // CREO UN PRODUCTO CON LA FUNCIÓN 'userArrayProducto($datos)' QUE ESTÁ DENTRO DE LA INSTANCIA '$productDb' DE LA CLASE 'JSONDB.PHP' HECHA EN 'LOADER.PHP'
+    $usersDb->saveProduct($producto); // GUARDO EL PRODUCTO CON LA FUNCIÓN 'saveProduct($user)' QUE ESTÁ DENTRO DE LA INSTANCIA '$usersDb' DE LA CLASE 'JSONDB.PHP' HECHA EN 'LOADER.PHP'
   }
 ?>
 
@@ -49,6 +49,7 @@ if ($_POST){
       </div>
       
   </form>
+  
 </div>
 
 </body>
