b808b360028e33b4657612d508abe46565cb0775
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-28 13:48:56 -0300

    nueva estetica

---
+++ nav-categoria.php
@@ -0,0 +1,20 @@
+<div class="nav-categoria">
+
+   <ul class="nav">
+        <li class="nav-item"><a class="nav-link active" href="#">EVENTOS</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">GASTRONOMÍA</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">TECNOLOGÍA</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">MÚSICA</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">DEPORTE</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">CONSTRUCCIÓN</a></li>
+    </ul>
+
+  </div>
+
+
+
+
+
+            
+
+
