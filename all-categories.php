6aa93623f37434284b85879c6e90f150df996180
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-30 04:58:50 -0300

    condicion para crear categorias linea 5a16

--- all-categories.php
+++ all-categories.php
@@ -2,6 +2,20 @@
 require 'DB/Connector.php';
 require 'DB/QueryBuilder.php';
 
+if ($_POST){
+    //   $errores = $validator->regValidate($_POST); // ACÁ VALIDO LOS ERRORES CON LA INSTANCIA '$validator' DE LA CLASE 'VALIDATOR.PHP' HECHA EN 'LOADER.PHP'
+    //   if(count($errores) == 0) {
+        
+        $name = $_POST['name'];
+        $descripcion = $_POST['descripcion'];
+        
+        $pdo = Connector::make();
+        $queryBuilder = new QueryBuilder($pdo);
+        $queryBuilder->createCategory($name, $descripcion);
+    //   }
+    }
+
+
 $pdo = Connector::make(); // agrego los datos de conección a la base de datos a la variable $pdo
 $queyBuilder = new QueryBuilder ($pdo); // creo una instancia del clase QueryBuilder 
                                         //que tiene funciones de consultas para la base y por eso le paso
