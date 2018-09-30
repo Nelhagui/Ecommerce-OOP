55c7eb88776ab26cf47bd5b228b76efd2a7a50ef
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-30 05:05:11 -0300

    incluir form para agregar categorias a db

--- views/all-categories-view.php
+++ views/all-categories-view.php
@@ -3,11 +3,13 @@ include 'loader.php'; // ESTE ARCHIVO CONTIENE LOS INCLUEDES DE LAS CLASES
     // QUE ANTES ESTABAN COMO FUNCIONES EN FUNCIONES.PHP
 
 if($auth->check()) {
-      $username  = $_SESSION['logged'];
+      $name  = $_SESSION['logged'];
 }
+include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()   
 
-include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()                   
 ?>
+
+
 
 
 
@@ -23,6 +25,25 @@ include 'helpers.php'; // ACÁ HAY FUNCIONES COMO EL ODL()
             <li><?=$categoria['name']; ?></li>
         <?php endforeach ?>
     </ul>
+
+
+        <div class="container-fluid"> 
+        <div class="row">
+            <form class="form-signin" action="" method="post">
+                <h2>Agregar Categoría</h2>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='name' placeholder="Ingresar nombre de Categoría">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" rows="5" id="comment" placeholder='Ingresar una descripción' name='descripcion'></textarea>
+                </div>                
+                <button type="submit" class="btn btn-primary">Agregar Categoría</button>
+            </form>
+        </div>
+    </div>
+
+    <?php include_once('footer.php'); ?>
+
 
 </body>
     
