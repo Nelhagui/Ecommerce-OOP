69300b5ee6a16c912cc6c8a0601a69fef3807d23
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-09-30 05:01:24 -0300

    agregue funcion crear usuario y crear category

--- DB/QueryBuilder.php
+++ DB/QueryBuilder.php
@@ -7,13 +7,34 @@ class QueryBuilder
     public function __construct (PDO $pdo) //construimos la variable $pdo con los datos de acceso a la base de datos
     {
         $this->pdo = $pdo;
-    }
 
-    public function index ($table)  // esta función sirve para consultar todo (*) de la base
+    }
+// esta función sirve para consultar todo (*) de la base
+    public function index ($table)  
     {
         $stmt = $this->pdo->prepare("SELECT * FROM {$table}"); // pongo la consulta en la variable $stmt
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC); // PDO::FETCH_ASSOC le dice que devuelva el resultado en una matriz asociativa
     }
+
+
+// funcion crea usuarios en la base de datos
+    public function createUser($username, $email, $pass, $genre)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO users (`id`, `username`, `email`, `pass`, `genre`, `imageuser`, `descripcion`) VALUES (NULL, '$username', '$email', '$pass', '$genre', '', '')");
+
+        $stmt->execute();
+    }
+
+
+// funcion crea categorías en la base de datos
+public function createCategory($name, $descripcion)
+{
+    $stmt = $this->pdo->prepare("INSERT INTO categories (`id`, `name`, `description`) VALUES (NULL, '$name', '$descripcion')");
+
+    $stmt->execute();
+}
+
+
 
 }
